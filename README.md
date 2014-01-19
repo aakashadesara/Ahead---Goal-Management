0e03bb7768e31700f93bbb33639992cfffa68c1c
Aakash Adesara <aakash.adesara@gmail.com>
2014-01-19 08:11:30 -0800

    Commit 11
    Commit after Aakash knocked out for a solid 4 hours

--- README.md
+++
@@ -1,4 +0,0 @@
-ahead
-=====
-
-Set goals with date/description then set how frequently you would like to be reminded about that goal by email, text messages, and/or social network notifications.
---
+++ goallist.php
@@ -0,0 +1,25 @@
+<?php
+
+// Database login info
+	$dbusername = "darapper_ahead";
+	$password = "aacodeday2014";
+	$hostname = "localhost"; 
+
+	// Connect to the database
+	$dbhandle = new mysqli($hostname, $dbusername, $password, "darapper_ahead") 
+	 or die("Unable to connect to MySQL");
+
+	// Select a database to work with
+	$selected = $dbhandle->select_db("darapper_ahead")
+	 or die("Could not select examples");
+
+
+
+	$query = "INSERT INTO goals values('$name','$description','$dateForDB','$frequency')";
+
+	$result = $dbhandle->query($query);
+
+
+
+
+?>
